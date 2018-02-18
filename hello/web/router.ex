cc2a5dba71f57c901c87622e2d54248d9a1510f3
quinnvaughn <qvaughn3@gmail.com>
2018-02-18 17:14:08 -0600

    First commit.

---
+++ hello/web/router.ex
@@ -0,0 +1,28 @@
+defmodule Hello.Router do
+  use Hello.Web, :router
+
+  pipeline :browser do
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
+  end
+
+  pipeline :api do
+    plug(:accepts, ["json"])
+  end
+
+  scope "/", Hello do
+    # Use the default browser stack
+    pipe_through(:browser)
+
+    get("/hello/:name", HelloController, :world)
+    get("/", PageController, :index)
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Hello do
+  #   pipe_through :api
+  # end
+end
